e85cfa524186568479a3054dccbf683de5a69233
Hotsse <dkdlrja@naver.com>
2020-02-10 00:12:14 +0900

    add import jquery samples

---
+++ import-jquery/main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+app.on('ready', () => {
+    win = new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadURL(`file://${__dirname}/index.html`)
+})
+
+app.on('closed', () => {
+    win = null
+})
