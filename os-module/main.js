ec75ac9882dc70a211b9def8bacefa1a1affd973
Hotsse <dkdlrja@naver.com>
2020-02-10 23:39:19 +0900

    add os-module post

---
+++ os-module/main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+var createWindow = function(){
+    win = new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL(`file://${__dirname}/index.html`)
+}
+
+app.on('ready', createWindow)
+
+app.on('closed', () => {
+    win = null
+})
